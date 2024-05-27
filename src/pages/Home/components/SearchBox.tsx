857845b0ac31943cece117b71833b9112cbe4923
Matt Eva <matteweva@gmail.com>
2024-05-27 16:19:39 -0700

    conditionally rendering search box

--- src/pages/Home/components/SearchBox.tsx
+++ src/pages/Home/components/SearchBox.tsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import SearchNav from "./SearchNav";
+import SimpleSearch from "./SimpleSearch";
+import AdvancedSearch from "./AdvancedSearch";
 
 function SearchBox() {
   const [simpleSearch, setSimpleSearch] = useState(true);
 
+  const toggleSearch = (bool: boolean) => {
+    setSimpleSearch(bool);
+  };
+
+  const search = simpleSearch ? <SimpleSearch /> : <AdvancedSearch />;
+
   return (
     <section>
-      <SearchNav />
+      <SearchNav toggleSearch={toggleSearch} />
+      {search}
     </section>
   );
 }
--- src/pages/Home/components/SearchNav.tsx
+++ src/pages/Home/components/SearchNav.tsx
@@ -1,8 +1,18 @@
-function SearchNav() {
+function SearchNav({ toggleSearch }: { toggleSearch: Function }) {
   return (
     <section className="flex justify-around w-full bg-red-50">
-      <button className="border-solid border-2 w-full">Simple Search</button>
-      <button className="border-solid border-2 w-full">Advanced Search</button>
+      <button
+        onClick={() => toggleSearch(true)}
+        className="border-solid border-2 w-full"
+      >
+        Simple Search
+      </button>
+      <button
+        onClick={() => toggleSearch(false)}
+        className="border-solid border-2 w-full"
+      >
+        Advanced Search
+      </button>
     </section>
   );
 }
