33ac3312561adecc3ef952dd2dbb66b9f751e980
Matt Eva <matteweva@gmail.com>
2024-05-27 15:53:03 -0700

    confirmed connection to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
@@ -5,6 +6,14 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const testFetch = async () => {
+      const res = await fetch("/api");
+      const data = await res.json();
+      console.log(data);
+    };
+    testFetch();
+  }, []);
   return (
     <div>
       <Header />
