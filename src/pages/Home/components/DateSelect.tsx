82623a2fcd62129dd3ef4d56b03b38883847d8fb
Matt Eva <matteweva@gmail.com>
2024-05-30 17:17:46 -0700

    extrapolated date select into separate component
    confirmed all simple search components are functional

---
+++ src/pages/Home/components/DateSelect.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function DateSelect({
+  date,
+  updateDate,
+}: {
+  date: string;
+  updateDate: Function;
+}) {
+  const handleChange = (e: React.ChangeEvent) => {
+    const target = e.target as HTMLSelectElement;
+    updateDate(target.value);
+  };
+  return (
+    <div>
+      <label>Date posted</label>
+      <select value={date} onChange={handleChange}>
+        <option>All time</option>
+        <option>Past Year</option>
+        <option>Past Month</option>
+        <option>Past Week</option>
+        <option>Past Day</option>
+        <option>Most Recent</option>
+      </select>
+    </div>
+  );
+}
+
+export default DateSelect;
--- src/pages/Home/components/SimpleSearch.tsx
+++ src/pages/Home/components/SimpleSearch.tsx
@@ -1,35 +1,33 @@
 import { useState } from "react";
-import WritingTypeSelect from "./GenreSelect/WritingTypeSelect";
+import WritingTypeSelect from "./WritingTypeSelect";
+import DateSelect from "./DateSelect";
 import GenreSelect from "./GenreSelect/GenreSelect";
 
+// all api queries from this component can be run periodically and cached
 function SimpleSearch() {
-  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
   const [writingType, setWritingType] = useState<string>("ShortStory");
+  const [date, setDate] = useState<string>("All time");
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
 
   const updateWritingType = (newType: string) => {
     setWritingType(newType);
+  };
+
+  const updateDate = (newDate: string) => {
+    setDate(newDate);
   };
 
   const updateSelectedGenres = (newGenres: string[]) => {
     setSelectedGenres(newGenres);
   };
 
-  console.log(selectedGenres);
   return (
     <section>
       <WritingTypeSelect
         writingType={writingType}
         updateWritingType={updateWritingType}
       />
-      <label>Date posted</label>
-      <select>
-        <option>All time</option>
-        <option>Past Year</option>
-        <option>Past Month</option>
-        <option>Past Week</option>
-        <option>Past Day</option>
-        <option>Most Recent</option>
-      </select>
+      <DateSelect date={date} updateDate={updateDate} />
       <GenreSelect
         selectedGenres={selectedGenres}
         updateSelectedGenres={updateSelectedGenres}
