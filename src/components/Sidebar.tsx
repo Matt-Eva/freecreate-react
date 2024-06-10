3a7ef6ceae3a27016fcc9ff195e70340594177e5
Matt Eva <matteweva@gmail.com>
2024-06-10 16:31:46 -0700

    added placeholder links to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,14 @@
 function Sidebar() {
   return (
     <aside className="hidden sm:block">
-      <nav>Sidebar</nav>
+      <nav>
+        <ul>
+          <li>Subscriptions</li>
+          <li>Read</li>
+          <li>Write</li>
+          <li>Library</li>
+        </ul>
+      </nav>
     </aside>
   );
 }
