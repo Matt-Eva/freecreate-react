50eee2b6cce1171e855cb03f3bf3576014f3c2ff
Matt Eva <matteweva@gmail.com>
2024-06-10 17:46:52 -0700

    added page level components to route file

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,10 @@
 import App from "./App";
 import Home from "./pages/Home/Home";
+import Library from "./pages/Library/Library";
+import About from "./pages/About/About";
+import Donate from "./pages/Donate/Donate";
+import Read from "./pages/Read/Read";
+import Write from "./pages/Write/Write";
 
 const routes = [
   {
@@ -9,6 +14,26 @@ const routes = [
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/donate",
+        element: <Donate />,
+      },
+      {
+        path: "/library",
+        element: <Library />,
+      },
+      {
+        path: "/read",
+        element: <Read />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
       },
     ],
   },
