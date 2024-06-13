51fb7dfc1229125a460bcb7b4126ecbdb75f02f3
Matt Eva <matteweva@gmail.com>
2024-06-12 17:10:55 -0700

    adjusted footer height - added background color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,23 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="sm:hidden">
-      <nav className="flex justify-evenly items-center h-full">
-        <Link to="/">browse</Link>
-        <Link to="/subscriptions">subscriptions</Link>
-        <Link to="/read">read</Link>
-        <Link to="/library">library</Link>
-        <Link to="/write">write</Link>
+    <footer className="sm:hidden bg-gray-50">
+      <nav className="grid grid-flow-col items-center h-full">
+        <Link to="/" className="text-center w-full border-r ">
+          browse
+        </Link>
+        <Link to="/subscriptions" className="text-center w-full border-r ">
+          subscriptions
+        </Link>
+        <Link to="/read" className="text-center w-full border-r ">
+          read
+        </Link>
+        <Link to="/library" className="text-center w-full border-r ">
+          library
+        </Link>
+        <Link to="/write" className="text-center w-full ">
+          write
+        </Link>
       </nav>
     </footer>
   );
