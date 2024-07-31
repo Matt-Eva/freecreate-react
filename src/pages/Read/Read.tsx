086d352df6f6d9c3a3c8f67004676fdf0cda463b
Matt Eva <matteweva@gmail.com>
2024-07-31 15:30:15 -0700

    removed unused class from Read

--- src/pages/Read/Read.tsx
+++ src/pages/Read/Read.tsx
@@ -3,10 +3,6 @@ import { useState } from "react";
 function Read() {
   const [isFullScreen, setIsFullScreen] = useState(false);
 
-  const buttonPosition = isFullScreen
-    ? "bottom-2 sm:top-2"
-    : "bottom-14 sm:top-20";
-
   const toggleFullScreen = () => {
     setIsFullScreen(!isFullScreen);
     const readContainer = document.getElementById("read-container");
