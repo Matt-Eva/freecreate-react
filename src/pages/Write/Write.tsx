d60993208c3443555f2521e4ca0a1e02a255256d
Matt Eva <matteweva@gmail.com>
2024-08-07 12:02:34 -0700

    set up layout for write nav

--- src/pages/Write/Write.tsx
+++ src/pages/Write/Write.tsx
@@ -1,9 +1,11 @@
 import { Outlet, Link } from "react-router-dom";
 
+import styles from "./Write.module.css";
+
 function Write() {
   return (
-    <div>
-      <nav>
+    <div className={styles.container}>
+      <nav className={styles.nav}>
         <Link to="/write">Drafts</Link>
         <Link to="/write/published">Published</Link>
         <Link to="/new-writing">New</Link>
