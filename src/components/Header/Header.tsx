175ffa6e0d76154bfe2384d64bc7f7f2d84d440a
Matt Eva <matteweva@gmail.com>
2024-08-15 09:33:06 -0700

    updated header to adjust layout for logged-out user

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,32 @@
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../store";
 
 import styles from "./Header.module.css";
 
 function Header() {
+  const userState = useAppSelector((state) => state.user.value);
   return (
     <header className={styles.header}>
       <h1>
         <Link to="/">FreeCreate</Link>
       </h1>
       <nav className={styles.navGrid}>
-        <Link to="/" className={styles.browse}>
-          browse
-        </Link>
+        {userState.authenticated ? null : (
+          <Link to="/" className={styles.browse}>
+            browse
+          </Link>
+        )}
         <Link to="/about">about</Link>
         <Link to="/donate">donate</Link>
-        <Link to="/profile">profile</Link>
+        {userState.isFetched ? (
+          userState.authenticated ? (
+            <Link to="/profile">profile</Link>
+          ) : (
+            <Link to="/login">login</Link>
+          )
+        ) : (
+          <div>loading...</div>
+        )}
       </nav>
     </header>
   );
