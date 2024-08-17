16edba696535bdccddf7d37f5183d8ae953396a5
Matt Eva <matteweva@gmail.com>
2024-08-17 16:26:51 -0700

    updated keys in default genre state
    extrapolated default genre state into function to avoid global variable pollution

--- src/pages/EditWriting/EditWriting.tsx
+++ src/pages/EditWriting/EditWriting.tsx
@@ -12,117 +12,14 @@ import styles from "./EditWriting.module.css";
 import Writing from "../../types/Writing";
 import { Link } from "react-router-dom";
 import GenreObject from "../../types/genreObject";
-
-const defaultGenreState: GenreObject = {
-  action: {
-    selected: false,
-    disabled: false,
-    value: "Action",
-    display: "Action",
-  },
-  adventure: {
-    selected: false,
-    disabled: false,
-    value: "Adventure",
-    display: "Adventure",
-  },
-  comedy: {
-    selected: false,
-    disabled: false,
-    value: "Comedy",
-    display: "Comedy",
-  },
-  drama: {
-    selected: false,
-    disabled: false,
-    value: "Drama",
-    display: "Drama",
-  },
-  historical: {
-    selected: false,
-    disabled: false,
-    value: "HistoricalFiction",
-    display: "Historical Fiction",
-  },
-  horror: {
-    selected: false,
-    disabled: false,
-    value: "Horror",
-    display: "Horror",
-  },
-  fantasy: {
-    selected: false,
-    disabled: false,
-    value: "Fantasy",
-    display: "Fantasy",
-  },
-  literary: {
-    selected: false,
-    disabled: false,
-    value: "LiteraryFiction",
-    display: "Literary Fiction",
-  },
-  magicalRealism: {
-    selected: false,
-    disabled: false,
-    value: "MagicalRealism",
-    display: "Magical Realism",
-  },
-  mystery: {
-    selected: false,
-    disabled: false,
-    value: "Mystery",
-    display: "Mystery",
-  },
-  realism: {
-    selected: false,
-    disabled: false,
-    value: "Realism",
-    display: "Realism",
-  },
-  romance: {
-    selected: false,
-    disabled: false,
-    value: "Romance",
-    display: "Romance",
-  },
-  sciFi: {
-    selected: false,
-    disabled: false,
-    value: "ScienceFiction",
-    display: "Science Fiction",
-  },
-  sliceOfLife: {
-    selected: false,
-    disabled: false,
-    value: "SliceOfLife",
-    display: "Slice of Life",
-  },
-  social: {
-    selected: false,
-    disabled: false,
-    value: "SocialFiction",
-    display: "Social Fiction",
-  },
-  superhero: {
-    selected: false,
-    disabled: false,
-    value: "Superhero",
-    display: "Superhero",
-  },
-  thriller: {
-    selected: false,
-    disabled: false,
-    value: "Thriller",
-    display: "Thriller",
-  },
-};
+import getDefaultGenreState from "../../state/defaultGenreState";
 
 function EditWriting() {
   const userState = useAppSelector((state) => state.user.value);
   const userCreatorState = useAppSelector((state) => state.userCreators.value);
   const editWritingState = useAppSelector((state) => state.editWriting.value);
 
+  const defaultGenreState: GenreObject = getDefaultGenreState();
   const [genres, setGenres] = useState<GenreObject>({ ...defaultGenreState });
   const [loadingCreators, setLoadingCreators] = useState(true);
   const [loadingWriting, setLoadingWriting] = useState(true);
@@ -210,10 +107,11 @@ function EditWriting() {
   }, []);
 
   function setExistingGenres(g: string[]) {
-    console.log(defaultGenreState);
-    const defaultGenres = { ...defaultGenreState };
+    console.log(genres);
+    const defaultGenres = { ...genres };
     g.forEach((genre) => {
-      const lower = genre.toLowerCase();
+      const lower = genre.charAt(0).toLowerCase() + genre.slice(1);
+      console.log(lower);
       defaultGenres[lower].selected = true;
     });
 
--- src/state/defaultGenreState.ts
+++ src/state/defaultGenreState.ts
@@ -1,106 +1,112 @@
 import GenreObject from "../types/genreObject";
 
-export const defaultGenreState: GenreObject = {
-  action: {
-    selected: false,
-    disabled: false,
-    value: "Action",
-    display: "Action",
-  },
-  adventure: {
-    selected: false,
-    disabled: false,
-    value: "Adventure",
-    display: "Adventure",
-  },
-  comedy: {
-    selected: false,
-    disabled: false,
-    value: "Comedy",
-    display: "Comedy",
-  },
-  drama: {
-    selected: false,
-    disabled: false,
-    value: "Drama",
-    display: "Drama",
-  },
-  historical: {
-    selected: false,
-    disabled: false,
-    value: "HistoricalFiction",
-    display: "Historical Fiction",
-  },
-  horror: {
-    selected: false,
-    disabled: false,
-    value: "Horror",
-    display: "Horror",
-  },
-  fantasy: {
-    selected: false,
-    disabled: false,
-    value: "Fantasy",
-    display: "Fantasy",
-  },
-  literary: {
-    selected: false,
-    disabled: false,
-    value: "LiteraryFiction",
-    display: "Literary Fiction",
-  },
-  magicalRealism: {
-    selected: false,
-    disabled: false,
-    value: "MagicalRealism",
-    display: "Magical Realism",
-  },
-  mystery: {
-    selected: false,
-    disabled: false,
-    value: "Mystery",
-    display: "Mystery",
-  },
-  realism: {
-    selected: false,
-    disabled: false,
-    value: "Realism",
-    display: "Realism",
-  },
-  romance: {
-    selected: false,
-    disabled: false,
-    value: "Romance",
-    display: "Romance",
-  },
-  sciFi: {
-    selected: false,
-    disabled: false,
-    value: "ScienceFiction",
-    display: "Science Fiction",
-  },
-  sliceOfLife: {
-    selected: false,
-    disabled: false,
-    value: "SliceOfLife",
-    display: "Slice of Life",
-  },
-  social: {
-    selected: false,
-    disabled: false,
-    value: "SocialFiction",
-    display: "Social Fiction",
-  },
-  superhero: {
-    selected: false,
-    disabled: false,
-    value: "Superhero",
-    display: "Superhero",
-  },
-  thriller: {
-    selected: false,
-    disabled: false,
-    value: "Thriller",
-    display: "Thriller",
-  },
-};
+function getDefaultGenreState() {
+  const defaultGenreState: GenreObject = {
+    action: {
+      selected: false,
+      disabled: false,
+      value: "Action",
+      display: "Action",
+    },
+    adventure: {
+      selected: false,
+      disabled: false,
+      value: "Adventure",
+      display: "Adventure",
+    },
+    comedy: {
+      selected: false,
+      disabled: false,
+      value: "Comedy",
+      display: "Comedy",
+    },
+    drama: {
+      selected: false,
+      disabled: false,
+      value: "Drama",
+      display: "Drama",
+    },
+    historicalFiction: {
+      selected: false,
+      disabled: false,
+      value: "HistoricalFiction",
+      display: "Historical Fiction",
+    },
+    horror: {
+      selected: false,
+      disabled: false,
+      value: "Horror",
+      display: "Horror",
+    },
+    fantasy: {
+      selected: false,
+      disabled: false,
+      value: "Fantasy",
+      display: "Fantasy",
+    },
+    literaryFiction: {
+      selected: false,
+      disabled: false,
+      value: "LiteraryFiction",
+      display: "Literary Fiction",
+    },
+    magicalRealism: {
+      selected: false,
+      disabled: false,
+      value: "MagicalRealism",
+      display: "Magical Realism",
+    },
+    mystery: {
+      selected: false,
+      disabled: false,
+      value: "Mystery",
+      display: "Mystery",
+    },
+    realism: {
+      selected: false,
+      disabled: false,
+      value: "Realism",
+      display: "Realism",
+    },
+    romance: {
+      selected: false,
+      disabled: false,
+      value: "Romance",
+      display: "Romance",
+    },
+    sciFi: {
+      selected: false,
+      disabled: false,
+      value: "ScienceFiction",
+      display: "Science Fiction",
+    },
+    sliceOfLife: {
+      selected: false,
+      disabled: false,
+      value: "SliceOfLife",
+      display: "Slice of Life",
+    },
+    socialFiction: {
+      selected: false,
+      disabled: false,
+      value: "SocialFiction",
+      display: "Social Fiction",
+    },
+    superhero: {
+      selected: false,
+      disabled: false,
+      value: "Superhero",
+      display: "Superhero",
+    },
+    thriller: {
+      selected: false,
+      disabled: false,
+      value: "Thriller",
+      display: "Thriller",
+    },
+  };
+
+  return defaultGenreState;
+}
+
+export default getDefaultGenreState;
