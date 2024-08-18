a603338880c99e116ebc609cf32f1bf2cdfe9790
Matt Eva <matteweva@gmail.com>
2024-08-17 18:47:48 -0700

    removing unused Link from writing info

--- src/components/WritingInfo/WritingInfo.tsx
+++ src/components/WritingInfo/WritingInfo.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import WritingTypeSelect from "../WritingTypeSelect/WritingTypeSelect";
 import GenreSelect from "../GenreSelect/GenreSelect";
 import TitleInput from "../TitleInput/TitleInput";
